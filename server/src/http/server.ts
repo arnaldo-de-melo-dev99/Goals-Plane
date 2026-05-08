25e70de0c11e60a19562d578019f9217f502bd87
ade-melo <arnaldodemlo99@gmail.com>
2026-05-08 18:55:19 +0100

    fix: promise functions(add async/ rout post => await createGoal)

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -5,7 +5,7 @@ import z from "zod";
 
 const app = fastify();
 
-app.post('/goals', (request) => {
+app.post('/goals', async request => {
     const createGoalBodySchema = z.object({
         title: z.string(),
         desiredWeeklyFrequency: z.number().int().min(1).max(7),
